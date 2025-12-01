fb60735b883b9b6e687ee13d53067539e28cb817
rudrayanikanmande24-hash <rudrayanikanmande24@gmail.com>
2025-12-01 23:40:35 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Itodo } from './table/models/table';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ourTableApp';
+  todoArr:Array<Itodo>=[]
+
+  POST_URL=`https://jsonplaceholder.typicode.com/todos`
+
+  constructor(
+    private _httpClient : HttpClient
+  ){}
+
+  ngOnInit(): void {
+    this._httpClient.get<Array<Itodo>>(this.POST_URL)
+    .subscribe((res:Array<Itodo>)=>{
+      console.log(res);
+      this.todoArr=res
+      
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
